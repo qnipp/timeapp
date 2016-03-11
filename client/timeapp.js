31c5bc26ce1065d004e4b0bb5abb02ba8e50c758
Andi Pauschenwein <andreas@qnipp.com>
2016-03-11 19:50:33 +0100

    [TASK] added collections. changed routes. layout fixed.

--- client/timeapp.js
+++ client/timeapp.js
@@ -1,42 +1,4 @@
 
-
-var OnBeforeRouteActions;
-
-OnBeforeRouteActions = {
-	loginRequired: function(pause) {
-		//console.log("iron:router - OnBeforeRouteActions:loginRequired ");
-		// if not loggedin - send to login page
-		if (!Meteor.userId()) {
-			Router.go('/');
-			//this.next();
-			//return pause();
-		} else {
-			//this.next();
-		}
-		this.next();
-	},
-	loginPrevented: function(pause) {
-		//console.log("iron:router - OnBeforeRouteActions:loginPrevented ");
-		// if already logged in - send to start page
-		if (Meteor.userId()) {
-			Router.go('/iamin');
-			//return pause();
-		} else {
-			//this.next();
-		}
-		this.next();
-	}
-};
-// redirect to login page 
-Router.onBeforeAction(OnBeforeRouteActions.loginRequired, {
-	except: ['login', 'logout']
-});
-// redirect to iamin page
-Router.onBeforeAction(OnBeforeRouteActions.loginPrevented, {
-	only: ['login']
-});
-
-
 Template.registerHelper('currentTemplateName', function() {
 	var currentTemplateName = Blaze.currentView.parentView.name;
 	//console.log(currentTemplateName);
--- server/server.js
+++ server/server.js
@@ -1,3 +1,38 @@
 Meteor.startup(function () {
     // code to run on server at startup
 }); 
+
+
+// publish read access to collections
+Meteor.publish("items.mine", function() {
+	return Items.find({
+		owner: this.userId
+	});
+});
+
+Meteor.publish("hierarchy.mine", function() {
+	return Hierarchy.find({
+		owner: this.userId
+	});
+});
+
+
+Meteor.publish("times.mine", function() {
+	return Times.find({
+		owner: this.userId
+	});
+});
+
+Meteor.publish("attribs.mine", function() {
+	return Attributes.find({
+		owner: this.userId
+	});
+});
+
+
+// coments on docs
+Meteor.publish("users", function() {
+	// TODO: only list users from group
+	// TODO: add group to users collection
+	return Users.find({});
+});
